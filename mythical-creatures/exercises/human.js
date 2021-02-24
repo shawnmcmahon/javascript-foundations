552da2484d48ad70cf39cfc3e805f8bf1dbd1632
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-02-23 21:16:26 -0700

    solve tests

---
+++ mythical-creatures/exercises/human.js
@@ -0,0 +1,19 @@
+class Human {
+  constructor (name) {
+    this.name = name;
+    this.encounterCounter = 0;
+    this.knockedOut = false;
+
+  }
+
+  noticesOgre () {
+    if (this.encounterCounter >= 3) {
+      this.encounterCounter = 0;
+      return true;
+    }
+  return false;
+  }
+
+}
+
+module.exports = Human;
