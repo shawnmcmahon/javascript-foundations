f43ab0a19e3e1967351339727695e5f7c4c9cc3c
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-02-24 21:35:43 -0700

    Add performIncantation and performTrick functions

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -2,7 +2,20 @@ class Magician{
   constructor(details) {
     this.name = 'The Great ' + details.name;
     this.assistant = details.assistant;
+    this.favoriteAccessory = details.clothing || 'top hat';
   }
+
+    performIncantation(incantation) {
+      var upperCaseIncantation = incantation.toUpperCase() + "!";
+      return upperCaseIncantation;
+    }
+
+    performTrick() {
+      var upperCaseClothing = this.favoriteAccessory.toUpperCase();
+      return `PULL RABBIT FROM ${upperCaseClothing}`;
+    }
+
+
 }
 
 module.exports = Magician;
