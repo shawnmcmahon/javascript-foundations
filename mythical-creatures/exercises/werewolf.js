58f47f5178f142e788379575addf9b7e83064a59
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-02-25 22:23:12 -0700

    Solve tests

--- mythical-creatures/exercises/werewolf.js
+++ mythical-creatures/exercises/werewolf.js
@@ -8,11 +8,24 @@ class Werewolf {
   completeTransformation() {
     if (this.form === "wolf") {
       this.form = 'human';
+      this.hungry = false;
       return 'Where are I?';
     } else {
       this.form = 'wolf';
+      this.hungry = true;
       return 'Aaa-Woooo!';
     }
+  }
+
+  eatVictim(victim) {
+    if (this.form === 'wolf') {
+      victim.alive = false;
+      this.form = 'human';
+      return `Yum, ${victim.name} was delicious.`
+    } else {
+      return `No way am I eating ${victim.name}, I'd like a burger!`
+    }
+
   }
 
 }
