493aca91279a329172deb11219cbef276388509e
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-02-27 12:29:07 -0700

    Fix location tests

--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -1,7 +1,7 @@
 class Stark{
   constructor(details) {
     this.name = details.name || 'Shawn';
-    this.location = details.location || 'Winterfell';
+    this.location = details.area || 'Winterfell';
     this.safe = false;
 
   }
