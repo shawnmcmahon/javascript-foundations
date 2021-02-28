3e8181cf28887d3dbacd45dc48591dbaf6e172b5
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-02-27 21:34:33 -0700

    Solve tests

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,4 +1,22 @@
 class Snowman {
+  constructor(details) {
+    // console.log(details)
+    this.carrots = details.carrots;
+    this.coal = details.coal;
+    this.buttons = details.buttons;
+    this.snowballs = details.snowballs;
+    this.magicHat = false;
+  }
+
+  canWearMagicHat() {
+    if (this.buttons >= 5 && this.coal >= 2) {
+      this.magicHat = true;
+    }
+    
+    if (this.carrots < 1 || this.snowballs < 2) {
+      this.magicHat = false;
+    }
+  }
 
 }
 
