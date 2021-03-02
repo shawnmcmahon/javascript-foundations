b5ae76165b1c8edc3b21017e7d7d5378ca82be29
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-01 19:28:36 -0700

    Solve gear.js

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,4 +1,20 @@
 class Gear {
+  constructor(type, quantity) {
+    this.type = type;
+    this.quantity = quantity;
+}
+
+  checkForValidity() {
+    if (this.type === 'food') {
+      return `Great, we\'ll need lots of ${this.type}!`;
+    } else if (this.type === 'clothes') {
+      return `Great, we\'ll need lots of ${this.type}!`;
+    } else if (this.type === 'ammunition') {
+      return `Great, we\'ll need lots of ${this.type}!`;
+    } else  {
+      return `I don\`t think a ${this.type} will help us.`;
+    }
+  }
 
 }
 
