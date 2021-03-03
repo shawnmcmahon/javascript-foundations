60bdf09db2a6a7dbd0cf66133f6192454a5f7dd6
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-02 20:59:25 -0700

    Solve first third

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,5 +1,10 @@
 class GolfCourse {
-
+  constructor(name, difficulty, openings, features) {
+    this.name = name;
+    this.difficulty = difficulty;
+    this.openings = openings;
+    this.features = features;
+  }
 
 }
 
