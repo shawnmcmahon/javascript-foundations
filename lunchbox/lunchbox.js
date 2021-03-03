cfd36bc2ba16256d0fe47cc99ad0742cc7add889
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-02 20:32:01 -0700

    Solve all tests

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,31 @@
 class LunchBox {
+  constructor(details) {
+    this.owner = details.owner;
+    this.material = details.madeOf;
+    this.shape = details.shape;
+    this.color = details.color;
+    this.snacks = [];
+  }
 
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    //console.log(this.snacks);
+    console.log(this.snacksfruitSnack);
+    //this.snacks[0].isInLunchBox = true;
+    snack.isInLunchBox = true;
+  }
+
+  findHealthySnacks() {
+    var healthySnacks = [];
+
+    for(var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        var healthySnackDetected = this.snacks[i].type;
+        healthySnacks.push(healthySnackDetected);
+      }
+    }
+    return healthySnacks;
+  }
 }
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,26 @@
 class Snack {
+  constructor(type) {
+    this.deliciousLevel = "extra";
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+  }
 
+  getEaten(){
+    this.amount -= 10;
+    if (this.amount <= 20){
+      this.cuttingItClose = true;
+    }
+  }
+
+  checkForHealthy() {
+    if (this.type.includes("fruit") || this.type.includes("Fruit")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = Snack;
