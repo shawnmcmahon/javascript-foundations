95d9ce73bb00934b0bb538467d2bb2243c453309
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-02 22:41:05 -0700

    Solve all but two tests

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -4,6 +4,15 @@ class GolfCourse {
     this.difficulty = difficulty;
     this.openings = openings;
     this.features = features;
+    this.currentlyPlaying = [];
+  }
+
+  checkInGroup(group){
+    if (this.openings > this.currentlyPlaying.length) {
+      this.currentlyPlaying.push(group.name);
+      var groupSize = group.length;
+      this.openings -= groupSize
+    }
   }
 
 }
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,4 +1,55 @@
 class Golfer {
+  constructor(details) {
+    this.name = details.name;
+    this.handicap = details.handicap;
+    this.openings = details.openings
+    this.frustration = 0;
+    this.confidence = 0;
+  }
+
+  calculateAvg(par) {
+    return `I usually shoot a ${par + this.handicap} on average.`;
+  }
+
+  playRound(golfCourse) {
+    if(golfCourse.difficulty === 'moderate') {
+      this.frustration += 100;
+    } else if (golfCourse.difficulty === 'hard') {
+      this.frustration += 500;
+    }
+  }
+
+  hitTheRange() {
+    this.confidence += 10;
+  }
+
+  marvel(golfCourse) {
+    return `I love the ${golfCourse.features[0]} and ${golfCourse.features[1]} on this course!`
+  }
+
+  whatYaShoot(score) {
+    if (score < 0) {
+      this.frustration -= 30
+      if(this.frustration < 0) {
+        this.frustration = 0;
+      }
+      return 'I AM THE GREATEST GOLFER ALIVE!'
+    } else if (score === 0) {
+      this.frustration -= 10
+      if(this.frustration < 0) {
+        this.frustration = 0;
+      }
+      return 'Booyah!'
+    } else {
+      this.frustration += 20
+      if(this.frustration < 0) {
+        this.frustration = 0;
+      }
+      return 'Doh!'
+    }
+
+    console.log(this.frustration);
+  }
 
 
 }
