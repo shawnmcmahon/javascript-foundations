9f659e1551e95074b620ad65baaedb5cdf3f6f62
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-03 21:23:08 -0700

    Solve 27 of 32 tests

--- oregon-trail/src/wagon.js
+++ oregon-trail/src/wagon.js
@@ -5,6 +5,7 @@ class Wagon {
     this.axles = details.axles || [];
     this.oxen = details.oxen || [];
     this.yokes = details.yokes || [];
+    this.settlers = details.settlers || [];
     this.food = details.food;
     this.ammunition = details.ammunition;
     this.clothes = details.clothes;
@@ -21,6 +22,19 @@ class Wagon {
     } else if (part.type === 'yoke') {
       this.yokes.push(part)
     }
+  }
+
+  canTravel() {
+    for (var i = 0; i < this.wheels.length; i++) {
+
+      if (this.wheels.length <= 3 || this.axles.length <= 1 ||
+          this.oxen.length <= 1 || this.yokes.length < 1 ||
+          this.settlers.length < 1 || this.wheels[i].broken) {
+            return false
+    }     else {
+        return true;
+    }
+  }
   }
 
 
