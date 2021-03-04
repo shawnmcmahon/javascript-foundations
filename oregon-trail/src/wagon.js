2109dd6221c94b29600c0a3f2683724641307020
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-03 21:06:30 -0700

    wagon.js

--- oregon-trail/src/wagon.js
+++ oregon-trail/src/wagon.js
@@ -1,24 +1,24 @@
 class Wagon {
   constructor(details) {
     this.title = details.name;
-    this.wheels = [];
-    this.axles = [];
-    this.oxen = [];
-    this.yokes = [];
+    this.wheels = details.wheels || [];
+    this.axles = details.axles || [];
+    this.oxen = details.oxen || [];
+    this.yokes = details.yokes || [];
     this.food = details.food;
     this.ammunition = details.ammunition;
     this.clothes = details.clothes;
   }
 
   addPart(part) {
-    //doesnt work because Part === an instance of a new Part. 
-    if (part === 'wheel') {
+    //doesnt work because Part === an instance of a new Part.
+    if (part.type === 'wheel') {
       this.wheels.push(part)
-    } else if (part === 'axles') {
+    } else if (part.type === 'axle') {
       this.axles.push(part)
-    } else if (part === 'oxen') {
+    } else if (part.type === 'ox') {
       this.oxen.push(part)
-    } else if (part === 'yokes') {
+    } else if (part.type === 'yoke') {
       this.yokes.push(part)
     }
   }
